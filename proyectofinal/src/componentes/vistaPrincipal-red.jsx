2e16f6e1a01b3a7bb292d0a29bcffa437315866c
jd1206 <julin.rodrguez@misena.edu.co>
2022-11-10 10:46:41 -0500

    News folder

--- proyectofinal/src/componentes/vistaPrincipal-red.jsx
+++ proyectofinal/src/componentes/vistaPrincipal-red.jsx
@@ -49,7 +49,28 @@ function VistaRedsocial({nombre}){
                             </div>
                         </div>
                         <div className="noticasCafe1 m-4">
-
+                            <h1 className="fs-5">Cafe</h1>
+                                <div id="carouselExampleControls" class="carousel slide carouselPrecio" data-bs-ride="carousel">
+                                    <div class="carousel-inner text-center fs-3">
+                                        <div class="carousel-item active">
+                                            $ 199.999
+                                        </div>
+                                        <div class="carousel-item">
+                                            $ 199.999
+                                        </div>
+                                        <div class="carousel-item">
+                                            $ 199.999
+                                        </div>
+                                    </div>
+                                    <button class="carousel-control-prev bg-dark rounded" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                        <span class="visually-hidden">Previous</span>
+                                    </button>
+                                    <button class="carousel-control-next bg-dark rounded" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                        <span class="visually-hidden">Next</span>
+                                    </button>
+                                </div>
                         </div>
                     </div>
                     <div className="contenidoRedsocial">
@@ -61,17 +82,56 @@ function VistaRedsocial({nombre}){
                         <div className="posts p-4 d-flex flex-column">
                             <div className="publicar rounded-4 p-4">
                                 <img src={require('../images/publicar.png')}alt='' width='50px' />
-                                
                             </div>
-                            <div className="cardsPost rounded-4 mt-2">
-                                <div></div>
+                            <div className="cardsPost rounded-4 mt-2 p-5">
+                                <div class="card mb-5">
+                                    <img src={require('../images/imageDefecto.png')} class="card-img-top"/>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                    </div>
+                                </div>
+                                <div class="card mb-5">
+                                    <img src={require('../images/imageDefecto.png')} class="card-img-top"/>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                    </div>
+                                </div>
+                                <div class="card mb-5">
+                                    <img src={require('../images/imageDefecto.png')} class="card-img-top"/>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                    </div>
+                                </div>
+                                <div class="card mb-5">
+                                    <img src={require('../images/imageDefecto.png')} class="card-img-top"/>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                    </div>
+                                </div>
+                                <div class="card mb-5">
+                                    <img src={require('../images/imageDefecto.png')} class="card-img-top"/>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                        
                     </div>
                 </div>
                 <div className="col">
-
+                    <div className="noticiasEmpleo m-4 rounded-4">
+                        <div></div>
+                    </div>
                 </div>
                 
             </div>
