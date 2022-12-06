16141e9924a73db63f6b183770c16c0ac47ea7fd
Juan Jose Valencia <juan.valencia927@misena.edu.co>
2022-12-06 11:00:30 -0500

    create publications
    se crea el hook para las publicaciones

---
+++ proyectofinal/src/hooks/api/publicaciones.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import axios from "axios";
+
+
+const publicaciones = (url) => {
+  const apiUrl = `${url}publication/`
+
+  const getAllPublication = async()=>{
+    try {
+        const res = await axios.get(apiUrl)
+        return res.data
+    } catch (error) {
+        return error.response.data        
+    }
+  }
+
+  const  getOnePublication = async(id)=>{
+    try {
+        const res = await axios.get(apiUrl+id)
+        return res.data
+    } catch (error) {
+        return error.response.data
+    }
+  }
+
+  const createPublication = async(id,title,content)=>{
+    let request = {title,content,user_id:id}
+    try {
+      const res = await axios.post(`${apiUrl}new`,request)
+      return res.data
+    } catch (error) {
+      return error.response.data 
+    }
+  }
+
+  const updatePublication = async(id,id_publication,title,content)=>{
+    let request = {title,content,user_id:id}
+    try {
+      const res = await axios.put(`${apiUrl}update/${id_publication}`,request)
+      return res.data
+    } catch (error) {
+      return error.response.data
+    }
+  }
+
+  const deletePublication = async(id_publication)=>{
+    try {
+      const res = await axios.delete(`${apiUrl}delete/${id_publication}`)
+      return res.data
+    } catch (error) {
+      return error.response.data
+    }
+  }
+
+  return {
+    getAllPublication,
+    getOnePublication,
+    createPublication,
+    updatePublication,
+    deletePublication,
+  }
+}
+
+export default publicaciones
